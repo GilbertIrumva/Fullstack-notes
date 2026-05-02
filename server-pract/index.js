fad59c65f1d808fa75ffc2e3865341956e9dd9a2
Gilbert Irumva <gilbert@opengates.app>
2026-05-02 16:09:01 +0300

    fix deployment setup

--- server-pract/index.js
+++ server-pract/index.js
@@ -1,39 +1,8 @@
 const express = require('express')
+const path = require('path')
 const app = express()
-
-let notes = [
-  {
-    id: '1',
-    content: 'HTML is easy',
-    important: true,
-  },
-  {
-    id: '2',
-    content: 'Browser can execute only JavaScript',
-    important: false,
-  },
-  {
-    id: '3',
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true,
-  },
-]
-
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-app.use(requestLogger)
-app.use(express.static('dist'))
 app.use(express.json())
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+app.use(express.static(path.join(__dirname, 'dist')))
 
 app.get('/api/notes', (request, response) => {
   response.json(notes)
@@ -49,12 +18,6 @@ app.get('/api/notes/:id', (request, response) => {
     response.status(404).end()
   }
 })
-
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0
-  return String(maxId + 1)
-}
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -83,11 +46,13 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
 
-app.use(unknownEndpoint)
+app.use((req, res) => {
+  res.status(404).json({ error: 'unknown endpoint' })
+})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
