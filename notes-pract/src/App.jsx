83141d42aa5ab010a6d5027ea9a66c4aa00bb230
Gilbert Irumva <gilbert@opengates.app>
2026-05-02 13:51:34 +0300

    remove dist tracking and update frontend

--- notes-pract/src/App.jsx
+++ notes-pract/src/App.jsx
@@ -1,19 +1,83 @@
-import { useEffect, useState } from "react"
-import notesService from "./services/notes"
+import { useState, useEffect } from 'react'
+import Footer from './components/Footer'
+import Note from './components/Note'
+import Notification from './components/Notification'
+import noteService from './services/notes'
 
-function App() {
+const App = () => {
   const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
-    notesService.getAll().then(data => {
-      setNotes(data)
+    noteService.getAll().then(initialNotes => {
+      setNotes(initialNotes)
     })
   }, [])
+
+  const addNote = event => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5
+    }
+
+    noteService.create(noteObject).then(returnedNote => {
+      setNotes(notes.concat(returnedNote))
+      setNewNote('')
+    })
+  }
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => (note.id !== id ? note : returnedNote)))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+
+  const handleNoteChange = event => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important)
 
   return (
     <div>
       <h1>Notes</h1>
-      {notes.map(note => (<div key={note.id}>{note.content} </div>))}
+      <Notification message={errorMessage} />
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+      <Footer />
     </div>
   )
 }
--- server-pract/index.js
+++ server-pract/index.js
@@ -1,251 +1,95 @@
-
 const express = require('express')
 const app = express()
-const cors = require('cors')
- 
 
-
-app.use(express.json())
-app.use(cors())
-app.use(express.static('dist'))
-
-
- let notes = [
-    {
-    id: "1",
-    content: "HTML is easy",
-    important: true
+let notes = [
+  {
+    id: '1',
+    content: 'HTML is easy',
+    important: true,
   },
   {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
+    id: '2',
+    content: 'Browser can execute only JavaScript',
+    important: false,
   },
   {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
+    id: '3',
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    important: true,
+  },
 ]
 
-app.get('/',(req, res)=>{
- res.send('<h1>Welcome to my sever</h1>')
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+app.use(express.static('dist'))
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (req,res)=>{
-  res.json(notes)
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
 })
 
-app.get('/api/notes/id/:id', (req, res )=>{
-  const id = Number(req.params.id)
+app.get('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  const note = notes.find((note) => note.id === id)
 
-  const ids = notes.find(n => n.id === id)
-  if (!ids) res.json({error: 'not found'})
-    res.json(ids)
-  
-})
-app.get('/api/notes/name/:name', (req, res )=>{
-  const name = req.params.name.toLowerCase()
-
-  const nms = notes.find(ns => ns.name === name)
-    if(!nms) res.json({error: 'name not exist'})
-    res.json(nms)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 })
 
-app.get('/api/notes/arrived/:value', (req, res) => {
-  const value = req.params.value == "true"
+const generateId = () => {
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0
+  return String(maxId + 1)
+}
 
-  const vals = notes.filter(vl => vl.arrived === value )
-  if (!vals) res.json({ error: 'no info' })
-  res.json(vals)
-})
+app.post('/api/notes', (request, response) => {
+  const body = request.body
 
-app.get('/api/notes/gender/:gender', (req, res )=>{
- const genda = req.params.gender
- const alg = notes.filter(a => a.gender === genda)
-  if (!alg) res.json({ error: 'no info' })
-
- res.json(alg)
-})
-
-app.delete('/api/notes/id/:id', (req, res)=>{
-    const id = Number(req.params.id)
-
-    const note = notes.find(n => n.id === id)
-
-    if (!note) res.json({error: 'delete unsuccessfull '})
-      notes = notes.filter(n => n.id !== id)
-    return res.json({message: 'delete successfull'})
-})
-
-app.put('/api/notes/id/:id', (req, res) => {
-  const body = req.body
-  const id = Number(req.params.id)
-
-  const note = notes.find(n => n.id === id)
-
-  if (!note) {
-    return res.json({ error: 'item not found' })
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing',
+    })
   }
 
-  const updated = { ...note, ...body }
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
 
-  notes = notes.map(n => n.id === id ? updated : n)
+  notes = notes.concat(note)
 
-  res.json(updated)
+  response.json(note)
 })
 
-app.post('/api/notes/', (req,res)=>{
-  const body = req.body
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  notes = notes.filter((note) => note.id !== id)
 
-  const generateId = () =>{
-    const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-    return maxId + 1
-  }
-  
-  const newNote = {
-    id: generateId(),  
-    name: body.name,
-    gender: body.gender,
-    arrived: body.arrived || false
-  }
-  notes = notes.concat(newNote)
-  res.json(notes)
+  response.status(204).end()
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
